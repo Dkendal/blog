46e947db98984492567d0500ff5fc8898cc753b4
Dylan Kendal <dylankendal@gmail.com>
2022-06-26 09:11:34 -0400

    add task file

--- quickstart/scripts/build/ox-hugo/watch.el
+++ quickstart/scripts/build/ox-hugo/watch.el
@@ -8,6 +8,9 @@
     (setq index-file "/app/org/index.org")
     (setq timer nil)
 
+    ; (dolist (file (file-expand-wildcards "/app/content/**/*"))
+    ;   (delete-file file))
+
     (defun export-md ()
       (find-file-read-only index-file)
       (org-hugo-export-wim-to-md :all-subtrees nil nil)
@@ -18,10 +21,10 @@
       (when (eq timer nil)
         (setq timer (run-at-time "1 sec" nil 'export-md))))
 
-  (export-md)
-  (file-notify-add-watch index-file '(change) 'file-change-callback)
-  (while t (read-event)))
+    (export-md)
+    (file-notify-add-watch index-file '(change) 'file-change-callback)
+    (while t (read-event)))
 
-(error
-  (message "Error: %s" err)
-  (kill-emacs 1)))
+  (error
+    (message "Error: %s" err)
+    (kill-emacs 1)))
